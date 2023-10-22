43df0fca4e5f509a2f5e528e0e6c97bc43fa954d
AakarshitChaurasia <71507490+AakarshitChaurasia@users.noreply.github.com>
2023-10-22 22:09:55 +0530

    Add files via upload

---
+++ SIP-CALACULATOR(Modified1)/SIP-1/SIP/View/HistoryView.swift
@@ -0,0 +1,42 @@
+
+//  HistoryView.swift
+
+//
+//  Created by Aakarshit Chaurasia on 16/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct HistoryView: View {
+    @Environment(\.presentationMode) var presentationMode
+    //@Binding var totalAmount: Double
+    
+    var body: some View {
+        VStack {
+            Text("History")
+            Spacer()
+            Text("Total Amount: ")//\(totalAmount)")
+            
+            Spacer()
+            Image(.thank)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(10)
+                        .padding()
+            Spacer()
+            Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                Text("Done").tint(Color.appTheme)
+            }
+        }
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryView()
+    }
+}
+
